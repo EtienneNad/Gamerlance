b22de68059108b9138b68d96fad7fe8e05bdc362
Étienne <lewisn12@hotmail.com>
2020-04-28 13:56:31 -0400

    projets Banque

---
+++ Programation_2/Banque/Banque/Clients.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banque
+{
+   public class Clients : IEquatable<Clients>
+        
+    {
+        private List<Compte> lesComptes = new List<Compte>();
+        private string nom;
+        private string prenom;
+
+        public Clients(string nom, string prenom)
+        {
+            this.Nom = nom ?? throw new ArgumentNullException(nameof(nom));
+            this.Prenom = prenom ?? throw new ArgumentNullException(nameof(prenom));
+            
+        }
+
+        public string Nom { get => nom; set => nom = value; }
+        public string Prenom { get => prenom; set => prenom = value; }
+
+        public int AjouterCompte(Compte compte)
+        {
+            lesComptes.Add(compte);
+            return lesComptes.Count;
+
+        }
+        public int SupprimerCompte(Compte compte)
+        {
+            foreach(Compte compte1 in lesComptes) { 
+            if (compte1.Equals(compte))
+            {
+            lesComptes.Remove(compte);
+            }
+            }
+            return lesComptes.Count;
+        }
+        public bool Depot(int montant, Compte compte)
+        {
+            foreach (Compte compte1 in lesComptes)
+            {
+                if (compte1.Equals(compte))
+                {
+                    compte1.Depot(montant);
+                    return true;
+                }
+            }
+            return false;
+        }
+        public bool Retrait(int montant, Compte compte)
+        {
+            foreach (Compte compte1 in lesComptes)
+            {
+                if (compte1.Equals(compte))
+                {
+                    compte1.Retrait(montant);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+            if (!(obj is Clients))
+            {
+                return false;
+            }
+            return Equals(obj as Clients);
+        }
+
+        public bool Equals(Clients other)
+        {
+            return other != null &&
+                Nom == nom &&
+                Prenom == prenom;
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = 1399261157;
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Nom);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Prenom);
+            return hashCode;
+        }
+    }
+}
---
+++ Programation_2/Banque/Banque/Compte.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banque
+{
+    public class Compte
+    {
+        private uint numCompte;
+        private ushort chiffreVerif;
+        private int montant;
+        
+
+        public Compte(uint noCompte, ushort chiffreVerif, int montant)
+        {
+            this.numCompte = noCompte;
+            this.chiffreVerif = chiffreVerif;
+            this.montant = montant;
+        }
+
+        public uint NoCompte { get => numCompte; set => numCompte = value; }
+        public ushort ChiffreVerif { get => chiffreVerif; set => chiffreVerif = value; }
+        public int Montant { get => montant; set => montant = value; }
+
+        public int Depot(int montant)
+        {
+            Montant += montant;
+            return Montant;
+        }
+        public int Retrait(int montant)
+        {
+            Montant -= montant;
+                return Montant;
+           
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+            if (!(obj is Compte))
+            {
+                return false;
+            }
+            return Equals(obj as Compte);
+        }
+
+        public bool Equals(Compte other)
+        {
+            return other != null &&
+                NoCompte == numCompte;
+        }
+    }
+}
